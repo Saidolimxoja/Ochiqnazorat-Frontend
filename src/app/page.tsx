e812a0bbb7bfe699b68007c51be27a918765af85
Saidolimxoja <saidolim3838@gmail.com>
2026-05-22 22:09:45 +0500

    ашч

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 'use client'
 
-import dynamic from 'next/dynamic'
 import { RequireSession } from '@/shared/auth/RequireSession'
-
-const AppShell = dynamic(() => import('@/modules/shell').then((mod) => mod.AppShell), {
-  ssr: false,
-})
+import { AppShell } from '@/modules/shell'
 
 export default function HomePage() {
   return (
